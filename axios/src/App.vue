c8456de236a40515236b77fcffdebfe5cd70a78d
ik_2dw3a <ik_2dw3a@H015110.NET>
2024-11-22 09:54:58 +0100

    axios

---
+++ axios/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>USUARIOS TRAIDOS</h1>
+  <ul>
+    <li v-for="user in users">
+      {{ user.name }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+  import axios from 'axios'
+  import { ref, onMounted } from 'vue';
+
+  const users = ref([])
+
+  onMounted(()=>{
+    cargarUsers()
+  })
+
+  const cargarUsers = async ()=>{
+    try{
+      const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users')
+      
+      users.value= respuesta.data
+      console.log(users)
+
+    }catch{
+
+    }
+  }
+
+</script>
