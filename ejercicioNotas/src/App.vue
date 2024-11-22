71bda233508c08a67755a2962302ffd372463c62
ik_2dw3a <ik_2dw3a@H015110.NET>
2024-11-22 09:28:10 +0100

    ejercicio notas

--- ejercicioNotas/src/App.vue
+++ ejercicioNotas/src/App.vue
@@ -2,26 +2,42 @@
   <h1>NOTAS</h1>
   <hr>
 
-  <input type="text" placeholder="Introduce una nota" v-model="nota"> <input type="submit" value="añadir" @click="añadirNota">
+  <input type="text" placeholder="Introduce una nota" v-model="nota"> 
+  <input type="submit" value="Añadir" @click="añadirNota">
   <br>
   <ul>
-    <li v-for=" (notaa, index) in notas">
-      {{ notaa }}
-      <button @click="eliminarNota">ELiminar</button>
+    <li v-for="(notaa, index) in notas" :key="index">
+      <input type="text" v-model="notaa.texto" :disabled="!notaa.editable">
+      <button @click="eliminarNota(index)">Eliminar</button>
+
+      <button @click="editarNota(index)">
+        {{ notaa.editable ? "Guardar" : "Editar" }}
+      </button>
     </li>
   </ul>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
-const nota = ref('')
-const notas = ref([])
+import { ref } from "vue";
 
-const añadirNota = ()=>{
-  notas.value.push(nota.value) 
-}
+const nota = ref('');
+const notas = ref([]);
+
+
+const añadirNota = () => {
+  //añado la nota con un campo 'editable'. para poder modificar el estado del cada input
+    notas.value.push({ texto: nota.value, editable: false });
+};
+
 
 const eliminarNota = (index) => {
-  notas.value.splice(index, 1); // Elimina 1 elemento en el índice especificado
+  // Elimina la nota en el indice
+  notas.value.splice(index, 1);
+};
+
+
+const editarNota = (index) => {
+  //cambio el valor del campo "editable"
+  notas.value[index].editable = !notas.value[index].editable;
 };
 </script>
