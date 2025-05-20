57588033a1eb65dc3b12910d0d5e884f259ee4ff
iimanol7 <149791709+iimanol7@users.noreply.github.com>
2025-05-20 15:40:33 +0200

    .

---
+++ basurto/src/stores/counter.js
@@ -0,0 +1,24 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useFestividadesStore = defineStore('festividades', {
+  state: () => ({
+
+    
+
+  }),
+
+
+  actions : {
+
+    
+
+  },
+
+  getters : {
+    
+    
+
+  }
+})
